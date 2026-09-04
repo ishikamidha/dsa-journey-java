c05d5157ab38527899ff0518001ffad287034e19
ishikamidha <ishu24208@gmail.com>
2026-09-04 14:21:03 +0530

    Time: 3 ms (17.86%), Space: 47.6 MB (76.27%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void moveZeroes(int[] nums) { 
+        int i=0;
+    
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]==0){
+                if(nums[j]!=0){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    i++;
+                }
+                
+            }
+            else{
+                i++;
+            }
+        }
+        
+        
+    }
+}
